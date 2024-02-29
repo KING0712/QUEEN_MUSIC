6b1984232326f9e25cdbad41079f59c9af7efb64
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-02-29 15:18:31 +0530

    Update Soundcloud.py

--- SHUKLAMUSIC/platforms/Soundcloud.py
+++ SHUKLAMUSIC/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from DAXXMUSIC.utils.formatters import seconds_to_min
+from SHUKLAMUSIC.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
