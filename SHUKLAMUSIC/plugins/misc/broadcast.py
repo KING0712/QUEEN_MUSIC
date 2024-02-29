b092766d87325b1612f51c45d4e14f70e810d25e
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-03-01 02:46:36 +0530

    Update broadcast.py

--- SHUKLAMUSIC/plugins/misc/broadcast.py
+++ SHUKLAMUSIC/plugins/misc/broadcast.py
@@ -117,7 +117,7 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_5"])
         text = _["broad_6"]
-        from DAXXMUSIC.core.userbot import assistants
+        from SHUKLAMUSIC.core.userbot import assistants
 
         for num in assistants:
             sent = 0
