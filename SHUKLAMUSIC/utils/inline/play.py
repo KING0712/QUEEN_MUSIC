b95f6927ea40c45b007e06445b9259a925ef81b2
Mr King <140832898+KING0712@users.noreply.github.com>
2024-03-10 16:16:07 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -67,8 +67,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-         InlineKeyboardButton(text="🍃 ᴏᴡɴᴇʀ 🍃", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="🍁 ɢʀᴏᴜᴘ 🍁", url=f"{SUPPORT_CHAT}",),
+         InlineKeyboardButton(text="🍃 𝐎ᴡɴᴇʀ 🍃", user_id=config.OWNER_ID),
+         InlineKeyboardButton(text="🍁 𝐆ʀᴏᴜᴘ 🍁", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -84,8 +84,8 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-         InlineKeyboardButton(text="🍃 ᴏᴡɴᴇʀ 🍃", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="🍁 ɢʀᴏᴜᴘ 🍁", url=f"{SUPPORT_CHAT}",),
+         InlineKeyboardButton(text="🍃 𝐎ᴡɴᴇʀ 🍃", user_id=config.OWNER_ID),
+         InlineKeyboardButton(text="🍁 𝐆ʀᴏᴜᴘ 🍁", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
