1b7abad28a9d96cc8bd552aa1f0ad9915c377f3c
Mr King <140832898+KING0712@users.noreply.github.com>
2024-03-10 15:19:15 +0530

    Update userbot.py

--- SHUKLAMUSIC/core/userbot.py
+++ SHUKLAMUSIC/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("SHIVANSH474")
-                await self.one.join_chat("MASTIWITHFRIENDSX")
+                await self.one.join_chat("l_ABOUT_l_KING_l")
+                await self.one.join_chat("l_ABOUT_l_KING_l")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("SHIVANSH474")
-                await self.one.join_chat("MASTIWITHFRIENDSX")
+                await self.two.join_chat("l_ABOUT_l_KING_l")
+                await self.one.join_chat("l_ABOUT_l_KING_l")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("SHIVANSH474")
-                await self.one.join_chat("MASTIWITHFRIENDSX")
+                await self.three.join_chat("l_ABOUT_l_KING_l")
+                await self.one.join_chat("l_ABOUT_l_KING_l")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("SHIVANSH474")
-                await self.one.join_chat("MASTIWITHFRIENDSX")
+                await self.four.join_chat("l_ABOUT_l_KING_l")
+                await self.one.join_chat("l_ABOUT_l_KING_l")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("MASTIWITHFRIENDSX")
-                await self.one.join_chat("SHIVANSH474")
+                await self.five.join_chat("l_ABOUT_l_KING_l")
+                await self.one.join_chat("l_ABOUT_l_KING_l")
             except:
                 pass
             assistants.append(5)
