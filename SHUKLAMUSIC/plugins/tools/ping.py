f96761587446bc03055c0d2d4e4c97adb426fd18
Mr King <140832898+KING0712@users.noreply.github.com>
2024-03-30 10:54:25 +0530

    Update ping.py

--- SHUKLAMUSIC/plugins/tools/ping.py
+++ SHUKLAMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        video="https://graph.org//file/5be677d49aa2e5c13cf75.jpg",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await SHUKLA.ping()
