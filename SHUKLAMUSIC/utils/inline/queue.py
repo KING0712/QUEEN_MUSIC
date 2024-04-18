73f71115b68402c9c6b3cf39c0ec08757870036a
❛𝐊ɩŋʛ𝅃𓆪ꪾ <140832898+KING0712@users.noreply.github.com>
2024-04-18 15:52:42 +0530

    Update queue.py

--- SHUKLAMUSIC/utils/inline/queue.py
+++ SHUKLAMUSIC/utils/inline/queue.py
@@ -67,10 +67,10 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="𝐁", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="𝐀", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="𝐁", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="𝐘", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
